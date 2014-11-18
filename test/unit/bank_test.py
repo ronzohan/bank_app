d87ae612f3ec8ec515f62ba650bdf4f40e55cdd7
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-18 19:29:26 +0800

    Added a test case where user tries to withdraw negative amount

--- test/unit/bank_test.py
+++ test/unit/bank_test.py
@@ -1,7 +1,8 @@
 import unittest
 from bank import Bank
 from account import Account
-from withdraw_amount_exceed_balance import WithdrawAmountExceedBalanceError
+from withdraw_amount_exceed_balance \
+    import WithdrawAmountExceedBalanceError, WithdrawAmountNegativeError
 
 
 class TestBank(unittest.TestCase):
@@ -47,4 +48,14 @@ class TestBank(unittest.TestCase):
 
         bank.add_account(account_1)
 
-        self.assertRaises(WithdrawAmountExceedBalanceError, bank.withdraw_balance, "001", 100)
+        self.assertRaises(WithdrawAmountExceedBalanceError,
+            bank.withdraw_balance, "001", 100)
+
+    def test_account_withdraw_negative_amount(self):
+        bank = Bank()
+        account_1 = Account("001", 50)
+
+        bank.add_account(account_1)
+
+        self.assertRaises(WithdrawAmountNegativeError,
+            bank.withdraw_balance, "001", -1)
