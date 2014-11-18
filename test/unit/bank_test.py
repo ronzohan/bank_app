b9967a84a257f294bcdc4f5f909081d9a30ff0db
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-18 18:29:50 +0800

    Changed account numbers from int to str

--- test/unit/bank_test.py
+++ test/unit/bank_test.py
@@ -7,8 +7,8 @@ class TestBank(unittest.TestCase):
     def test_add_account(self):
         bank = Bank()
 
-        account_1 = Account(001, 50)
-        account_2 = Account(002, 20)
+        account_1 = Account("001", 50)
+        account_2 = Account("002", 20)
 
         bank.add_account(account_1)
         bank.add_account(account_2)
@@ -18,16 +18,16 @@ class TestBank(unittest.TestCase):
     def test_get_account_balance(self):
         bank = Bank()
 
-        account_1 = Account(001, 50)
+        account_1 = Account("001", 50)
 
         bank.add_account(account_1)
 
-        self.assertEqual(bank.get_account_balance(001), 50)
+        self.assertEqual(bank.get_account_balance("001"), 50)
 
     def test_account_number_exists(self):
         bank = Bank()
-        account_1 = Account(001, 50)
+        account_1 = Account("001", 50)
 
         bank.add_account(account_1)
-        self.assertRaises(KeyError, bank.get_account_balance, 002)
+        self.assertRaises(KeyError, bank.get_account_balance, "002")
 
