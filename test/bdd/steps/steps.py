10ab5bfd182b68c05687af331b696b8b2cd5d165
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-21 20:44:43 +0800

    Added scenario  'When I enter the account number 1111'

--- test/bdd/steps/steps.py
+++ test/bdd/steps/steps.py
@@ -1,7 +1,20 @@
-from lettuce import step
+from lettuce import step, world
 from nose.tools import assert_equal
 from webtest import TestApp
 
+from bank_app import app
+
+
 @step(u'I visit the homepage')
 def i_visit_the_homepage(step):
-    assert True, 'This step must be implemented'
+    world.browser = TestApp(app)
+    world.response = world.browser.get('http://localhost:5000/')
+    assert_equal(world.response.status_code, 200)
+
+
+@step(u'I enter the account number "([^"]*)"')
+def when_i_enter_the_account_number_group1(step, account_number):
+    form = world.response.forms['account-form']
+    form['account_number'] = account_number
+    world.form_response = form.submit()
+    assert_equal(world.form_response.status_code, 200)
