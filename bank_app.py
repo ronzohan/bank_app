50d23733c1ca8e064269570aa28e540aba714cb6
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2014-11-21 22:11:37 +0800

    Updated function to get account number from args

--- bank_app.py
+++ bank_app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from bank import Bank
+from account import Account
 
 app = Flask(__name__)
+BANK = Bank()
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    account_number = request.args.get('account_number')
+    balance = BANK.get_account_balance(account_number)
+    return render_template('index.html', balance=balance)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    account = Account('1111', 50)
+    BANK.add_account(account)
     app.run(debug=True)
