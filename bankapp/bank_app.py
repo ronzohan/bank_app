fdca33a83d0f9901876a3ea08b591dc30464df92
Ron Daryl Magno <ron.zohan@yahoo.com>
2014-12-04 20:05:34 +0800

    Followed PEP-8 defined standard

--- bankapp/bank_app.py
+++ bankapp/bank_app.py
@@ -1,18 +1,24 @@
+"""
+Controller file
+"""
+
 from flask import Flask, render_template, request
 from bankapp.bank import Bank
 from bankapp.account import Account
 
-app = Flask(__name__)
+APP = Flask(__name__)
 BANK = Bank()
 
-
-@app.route('/')
+@APP.route('/')
 def hello_world():
+    """
+    View for the index.html
+    """
     account_number = request.args.get('account_number')
     balance = BANK.get_account_balance(account_number)
     return render_template('index.html', balance=balance)
 
 if __name__ == '__main__':
-    account = Account('1111', 50)
-    BANK.add_account(account)
-    app.run(debug=True)
+    ACCOUNT = Account('1111', 50)
+    BANK.add_account(ACCOUNT)
+    APP.run(debug=True)
