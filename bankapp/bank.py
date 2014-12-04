45630a570015ebe9ba08e6b398d0af9e681d918a
Ron Daryl Magno <ron.zohan@yahoo.com>
2014-12-04 20:13:05 +0800

    Followed PEP8 defined standard

--- bankapp/bank.py
+++ bankapp/bank.py
@@ -1,15 +1,28 @@
-from withdraw_amount_exceed_balance \
+"""
+Model for the bank object
+"""
+
+from bankapp.withdraw_amount_exceed_balance \
     import WithdrawAmountExceedBalanceError, WithdrawAmountNegativeError
 
 
 class Bank(object):
+    """
+    Bank class
+    """
     def __init__(self):
         self.accounts = {}
 
     def add_account(self, account):
+        """
+        Add an account in the bank
+        """
         self.accounts[account.account_number] = account.balance
 
     def get_account_balance(self, account_no):
+        """
+        Retrieve balance from an account number
+        """
         balance = self.accounts.get(account_no)
         if balance is None:
             return 'No account associated with that account'
@@ -17,6 +30,9 @@ class Bank(object):
             return balance
 
     def withdraw_balance(self, account_no, amount):
+        """
+        Withdraws an amount of money from an account
+        """
         balance = self.accounts.get(account_no)
 
         if amount < 0:
--- bankapp/withdraw_amount_exceed_balance.py
+++ bankapp/withdraw_amount_exceed_balance.py
@@ -1,5 +1,15 @@
+"""
+Classes of errors
+"""
+
 class WithdrawAmountExceedBalanceError(Exception):
+    """
+    Error for withdrawing amount more than the balance
+    """
     pass
 
 class WithdrawAmountNegativeError(Exception):
+    """
+    Error for withdrawing negative amount
+    """
     pass
