94991159eac8b18082e068a62abb4f28722bcecc
Ron Daryl Magno <ron.zohan@yahoo.com>
2014-12-08 18:33:26 +0800

    Changed variable app to all caps since it is global

--- test/bdd/steps/steps.py
+++ test/bdd/steps/steps.py
@@ -2,11 +2,11 @@ from lettuce import *
 from nose.tools import assert_equal, assert_in
 from webtest import TestApp
 from bankapp.account import Account
-from bankapp.bank_app import app, BANK
+from bankapp.bank_app import APP, BANK
 
 @step(u'I visit the homepage')
 def i_visit_the_homepage(step):
-    world.browser = TestApp(app)
+    world.browser = TestApp(APP)
     world.response = world.browser.get('http://localhost:5000/')
     assert_equal(world.response.status_code, 200)
 
